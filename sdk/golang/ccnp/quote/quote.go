a1556a230cebd33725d371ccb868725f54f641a3
hairongchen <hairong.chen@intel.com>
2023-09-22 13:33:31 +0800

    change file name

---
+++ sdk/golang/ccnp/quote/quote.go
@@ -0,0 +1,41 @@
+package quote
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/hairongchen/confidential-cloud-native-primitives/tree/quote_server_go_sdk_dev/sdk/golang/ccnp/quote/proto"
+)
+
+const (
+	UDS_PATH = "unix:/run/ccnp/uds/quote-server.sock"
+	TYPE_TDX = "TDX"
+	TYPE_TPM = "TPM"
+)
+
+func GetQuote(user_data string, nonce string) (string, error) {
+
+	conn, err := grpc.Dial(UDS_PATH, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGetQuoteClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.GetQuote(ctx, &pb.GetQuoteRequest{UserData: "", Nonce: ""})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Response quote type: %s", r.QuoteType)
+	log.Printf("Response quote: %s", r.Quote)
+
+	return r.QuoteType, nil
+}
